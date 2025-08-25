a9447178f276464a1b57d6de7307a6ff17e6fdfb
Lucas Tjor <tjor75@gmail.com>
2025-08-25 11:28:16 -0300

    Actualizar main.jsx con WeatherApp.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import WeatherApp from './WeatherApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <WeatherApp />
   </StrictMode>,
 )
